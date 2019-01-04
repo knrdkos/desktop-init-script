ec6da70837a2e1daef68aecc6d2b085f3e50d01f
Konrad Kostrzewa <knrd.kostrzewa@gmail.com>
2019-01-04 16:21:18 +0100

    added snaps, readme and scripts copy to bin directory

---
+++ README.md
@@ -0,0 +1,7 @@
+Setup script for my desktop on Xubuntu 18.04 
+
+Should be run after clean os install.
+
+```
+wget https://raw.githubusercontent.com/knrdkos/desktop-scripts/master/initial.sh && chmod +x initial.sh && sudo bash initial.sh
+```
--- initial.sh
+++ initial.sh
@@ -6,12 +6,21 @@ then
     exit 1
 fi
 
-add-apt-repository -y  ppa:papirus/papirus 
-add-apt-repository -y ppa:webupd8team/java 
+add-apt-repository -y ppa:papirus/papirus
+add-apt-repository -y ppa:webupd8team/java
 apt update && apt upgrade -y
+
 apt install -y zim keepassx arc-theme papirus-icon-theme i3lock vim wmctrl \
 	scrot xclip htop steam linux-tools-common parcellite linux-tools-generic \
-       	network-manager-openvpn-gnome neofetch vlc gnome-disk-utility stress maven git docker.io xfce4-goodies
+       	network-manager-openvpn-gnome neofetch vlc gnome-disk-utility stress maven \
+       	git docker.io xfce4-goodies
+
+gpasswd -a $SUDO_USER docker
+
 snap install spotify pdftk telegram-desktop
+snap install --classic intellij-idea-ultimate
+
+git clone https://github.com/knrdkos/desktop-scripts.git
+mv desktop-scripts/*.sh /usr/local/bin/
 
 apt install -y oracle-java8-installer
