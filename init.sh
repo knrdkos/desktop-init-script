338d571924a8f0b226bf7c29646c701038dd510b
Konrad Kostrzewa <knrd.kostrzewa@gmail.com>
2020-01-21 20:13:30 -0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -12,18 +12,16 @@ echo "If You've used command from github, system should reboot after succesfull 
 REPO_NAME=desktop-init-script
 
 add-apt-repository -y ppa:papirus/papirus
-add-apt-repository -y ppa:webupd8team/java
 apt update && apt upgrade -y
 
 apt install -y zim keepassx arc-theme papirus-icon-theme i3lock vim wmctrl \
-	scrot xclip htop steam linux-tools-common parcellite linux-tools-generic \
+	scrot xclip htop linux-tools-common parcellite linux-tools-generic \
        	network-manager-openvpn-gnome neofetch vlc gnome-disk-utility stress maven \
        	git docker.io xfce4-goodies
 
 gpasswd -a $SUDO_USER docker
 
-snap install spotify pdftk telegram-desktop
-snap install --classic intellij-idea-ultimate
+snap install spotify pdftk
 
 git clone https://github.com/knrdkos/$REPO_NAME.git
 mv $REPO_NAME/usr_local_bin/*.sh /usr/local/bin/
@@ -38,10 +36,6 @@ mv $REPO_NAME/xfce_conf/panel/*  $XFCE_CONFIG_FOLDER/panel/
 
 echo "alias c='xclip -selection clipboard'" >> /home/$SUDO_USER/.bashrc
 mkdir -p /home/$SUDO_USER/.ssh
-
-# silent oracle installation - possibly migrate to openjdk later?
-echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-apt install -y oracle-java8-installer
 
 echo "Script will attempt a cleanup and reboot in:"
 secs=5
